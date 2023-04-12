d9b3fc5a559d54173b5990732fc63108673f92e6
munkhd31g3r3kh <munkhd31g3r3kh@gmail.com>
2023-04-12 16:37:08 +0900

    matching with PCAN tracer

--- read_test.py
+++ read_test.py
@@ -6,14 +6,14 @@ import datetime
 
 cols = ["No", "Time_Offset", "Type", "ID", "Data_Length", 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight']
 x = datetime.datetime.now()
-x = "W:\\New_ERA\\CAN_message_tool\\Dataset\\Replay_attack_dataset_" + str(x).split()[0] + "-1.txt"
+x = "C:\\Users\\LISA\\Desktop\\뭉흐 공격 도구\\Fuzzing Attack\\Fuzzing Attack Injected Datas " + str(x).split()[0] + ".txt"
 f = open(x, "a")
 
 # all_data = "No\tID\tTYPE\tLEN\tONE\tTWO\tTHREE\tFOUR\tFIVE\tSIX\tSEVEN\tEIGHT\n"
 all_data = ""
 for col in cols:
-    all_data += col + "  "
-all_data += "\n"
+    all_data += col + "\t"
+
 print(all_data)
 f.write(all_data)
 def update_progress(progress):
@@ -40,16 +40,21 @@ if __name__ == "__main__":
             # print("PCAN-USB Pro FD (Ch-1) was initialized")
             mess = CAN.Read(CAN_BUS)
             if hex(mess[1].ID) != "0x0":
-                all_data = str(ind) + ')' + "\t"
+                all_data = ind + ')' + "\t"
                 offset = (time.time() - start_time)*1000
                 all_data += "{:.1f}".format(offset) + "\t"
-                all_data += str(mess[1].MSGTYPE) + "\t" + str(hex(mess[1].ID)) + "\t" + str(hex(mess[1].LEN))  
+                id_hex = hex(mess[1].ID)[2:]
+                for _ in range(4 - len(id_hex)):
+                    id_hex = '0' + id_hex.upper()
+                all_data += str(mess[1].MSGTYPE) + "\t" + id_hex + "\t" + str(hex(mess[1].LEN)[2:]) 
                 for j in range(mess[1].LEN):
-                    all_data += "\t" + str(hex(mess[1].DATA[j]))
+                    data_hex = hex(mess[1].DATA[j])[2:]
+                    for _ in range(2 - len(data_hex)):
+                        data_hex = '0' + data_hex.upper()
+                    all_data += "\t" + data_hex
                 all_data += "\n"
                 f.write(all_data)
                 print(all_data)
-                ind += 1
 
     # All initialized channels are released
     CAN.Uninitialize(PCAN_NONEBUS)
